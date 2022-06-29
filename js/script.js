055c8cb841fd00b43eaebb33b934390b9d93ea1a
Dmytro Oparivsky <kristobalkomon@gmail.com>
2022-06-29 19:57:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+   $('.slider__inner, .news__slider-items').slick({
+      prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+      nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+      infinite: false,
+   });
+});
+
